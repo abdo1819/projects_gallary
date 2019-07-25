bef4f674d7ae952a0a162875a5c5864bc4baaaf1
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-07-25 22:55:25 +0200

    add model for core

--- viewer_core/models.py
+++ viewer_core/models.py
@@ -1,23 +1,39 @@
 from django.db import models
 
-class Department(models.Model):
-    name = models.CharField(max_length=200)
 
 
 class Branch(models.Model):
-    name = models.CharField(max_legth=200)
+    name = models.CharField(max_length=200)
 
 
 class Instractor(models.Model):
     name = models.CharField(max_length=200)
-    branch = models.ForeignKey(Branch on_delete=models.CASCADE)
     e_mait = models.EmailField()
+    img = models.ImageField(null=True,upload_to="img/instractor/")
+    project = models.ManyToManyField('Project')
 
-class student(models.Model):
+
+
+class Student(models.Model):
     name = models.CharField(max_length=200)
     e_mait = models.EmailField()
-    branch = models.ForeignKey(Branch on_delete=models.CASCADE)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    img = models.ImageField(null=True,upload_to="img/student/")
+    project = models.ManyToManyField('Project')
+    
 
 
-class year(models.Model):
+class Tag(models.Model):
     name = models.CharField(max_length=200)
+    project = models.ManyToManyField('Project')
+
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    img = models.ImageField(null=True, upload_to="img/project/")
+    description = models.TextField()
+    markdown = models.TextField()
+    branch = models.ManyToManyField('Branch')
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    
