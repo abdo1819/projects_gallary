7973769fc692bad41c2df0088df0b2d80de1ec23
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-07-25 23:42:26 +0200

    move instractor and student to project for easier admin tools

--- viewer_core/admin.py
+++ viewer_core/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Student,Branch,Instractor,Project,Tag
+
+admin.site.register(Student)
+admin.site.register(Branch)
+admin.site.register(Instractor)
+admin.site.register(Project)
+admin.site.register(Tag)
--- viewer_core/models.py
+++ viewer_core/models.py
@@ -9,8 +9,8 @@ class Branch(models.Model):
 class Instractor(models.Model):
     name = models.CharField(max_length=200)
     e_mait = models.EmailField()
-    img = models.ImageField(null=True,upload_to="img/instractor/")
-    project = models.ManyToManyField('Project')
+    img = models.ImageField(null=True,upload_to="img/instractor/", blank=True)
+    
 
 
 
@@ -18,15 +18,12 @@ class Student(models.Model):
     name = models.CharField(max_length=200)
     e_mait = models.EmailField()
     branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
-    img = models.ImageField(null=True,upload_to="img/student/")
-    project = models.ManyToManyField('Project')
+    img = models.ImageField(null=True,upload_to="img/student/", blank=True)
     
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=200)
-    project = models.ManyToManyField('Project')
-
 
 
 class Project(models.Model):
@@ -35,7 +32,10 @@ class Project(models.Model):
     description = models.TextField()
     markdown = models.TextField()
     branch = models.ManyToManyField('Branch')
-    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+    tags = models.ManyToManyField('Tag')
+    students = models.ManyToManyField('Student')
+    instractors = models.ManyToManyField('Instractor')
     
     def has_parent(self):
         if self.parent is None:
