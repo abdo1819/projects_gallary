f5ad2f911f63867887ac0e1d84feb02014f666dd
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-07-30 04:19:28 +0200

    add groub to database & fix typo in email

--- viewer_core/models.py
+++ viewer_core/models.py
@@ -4,14 +4,14 @@ from django.db import models
 
 class Branch(models.Model):
     name = models.CharField(max_length=200)
-   
+
     def __str__(self):
         return self.name
 
 
 class Instractor(models.Model):
     name = models.CharField(max_length=200)
-    e_mait = models.EmailField()
+    e_mail = models.EmailField()
     img = models.ImageField(null=True,upload_to="img/instractor/", blank=True)
 
     def __str__(self):
@@ -22,8 +22,8 @@ class Instractor(models.Model):
 
 class Student(models.Model):
     name = models.CharField(max_length=200)
-    e_mait = models.EmailField()
-    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+    e_mail = models.EmailField()
+    group = models.ForeignKey('Group', on_delete=models.CASCADE)
     img = models.ImageField(null=True,upload_to="img/student/", blank=True)
 
     def __str__(self):
@@ -37,21 +37,30 @@ class Tag(models.Model):
     def __str__(self):
         return self.name
 
+class Group(models.Model):
+    name = models.CharField(max_length=200)
+    branch = models.ForeignKey(Branch, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
     img = models.ImageField(null=True, upload_to="img/project/")
     description = models.TextField()
     markdown = models.TextField()
-    branch = models.ManyToManyField('Branch')
+
     parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    group = models.ManyToManyField('Group')
     tags = models.ManyToManyField('Tag')
     students = models.ManyToManyField('Student')
     instractors = models.ManyToManyField('Instractor')
 
     def __str__(self):
         return self.title
-    
+
     def has_parent(self):
         if self.parent is None:
             return False
