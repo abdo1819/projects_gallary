e897786431d30f81d588feae98283da142e12ae5
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-07-30 05:25:23 +0200

    add groub to admin

--- viewer_core/admin.py
+++ viewer_core/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student,Branch,Instractor,Project,Tag
+from .models import Student,Branch,Instractor,Project,Tag,Group
 
 admin.site.register(Student)
 admin.site.register(Branch)
 admin.site.register(Instractor)
 admin.site.register(Project)
 admin.site.register(Tag)
+admin.site.register(Group)
