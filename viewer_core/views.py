b44a0758bb2b8e05f834fd00a05fd5e0ed00b745
abdelrahman ragab hashem <ar1813@fayoum.edu.eg>
2019-08-10 15:51:00 +0200

    add sample url

--- viewer_core/views.py
+++ viewer_core/views.py
@@ -7,6 +7,7 @@ from .models import Project,Branch,Group
 def hello(request):
     return HttpResponse("You're looking at hello .")
 
+# http://127.0.0.1:8000/computer/forth
 def projects(request, branch_name, group_name):
     project_branch = Branch.objects.get(name=branch_name) 
     project_group  = Group.objects.get(name=group_name, branch=project_branch)
